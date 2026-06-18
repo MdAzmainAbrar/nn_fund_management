32518dbf7e2f11bae45f5177cb7dd9985725ac9c
MdAzmainAbrar <abrarmain.aa@gmail.com>
2026-06-18 07:09:17 +0600

    feat: Add security access rules and application icon for Fund Management

--- nn_fund_management/__manifest__.py
+++ nn_fund_management/__manifest__.py
@@ -4,10 +4,12 @@
     'category': 'Accounting/Finance',
     'summary': 'Manage incoming funds, allocations, requisitions and transfers',
     'author': 'Md Azmain Abrar',
+    'license': 'LGPL-3',
     'depends': ['base', 'mail', 'web'],
     'data': [
-        'views/fund_account_view.xml',
-    ],
+    'security/ir.model.access.csv',
+    'views/fund_account_view.xml',
+],
     'installable': True,
     'application': True,
 }
